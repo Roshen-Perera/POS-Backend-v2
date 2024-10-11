48a0e1ce7c5e65554032cee70ea08f62db04a1b8
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-10-11 19:57:27 +0530

    can update customer by id

--- src/main/java/lk/ijse/posbackendv2/controller/CustomerController.java
+++ src/main/java/lk/ijse/posbackendv2/controller/CustomerController.java
@@ -50,6 +50,28 @@ public class CustomerController {
     @GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public CustomerStatus getSelectedCustomer(@PathVariable ("userId") String userId){
         return userService.getCustomer(userId);
+    }
 
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> update(
+            @RequestPart("customerId") String customerId,
+            @RequestPart("name") String name,
+            @RequestPart("address") String address,
+            @RequestPart("phone") String phone
+    )
+    {
+        try {
+            var buildCustomerDTO = new CustomerDTO();
+            buildCustomerDTO.setId(customerId);
+            buildCustomerDTO.setName(name);
+            buildCustomerDTO.setAddress(address);
+            buildCustomerDTO.setPhone(phone);
+            userService.saveCustomer(buildCustomerDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
